9bfa8da4615f6ee6d26345afb08fac877eb1af70
7embola-py <yahyamorchid64@gmail.com>
2025-11-15 22:21:05 +0100

    Add permission checks to book views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/list_books.html', {'books': books})
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    return HttpResponse("Create book page (only users with can_create).")
+
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return HttpResponse(f"Edit page for {book.title} (requires can_edit).")
+
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return HttpResponse(f"Delete page for {book.title} (requires can_delete).")
+
