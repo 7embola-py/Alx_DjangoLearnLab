4927c25068c83337e533f56eae6000ac334a9b06
7embola-py <yahyamorchid64@gmail.com>
2025-11-15 16:51:53 +0100

    Create CustomUser model extending AbstractUser
    Added a custom user model with date of birth and profile photo fields.

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+# Book Model
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
