cf3eddb1596832242bc554da2d081d4886e907f4
7embola-py <yahyamorchid64@gmail.com>
2025-11-15 22:25:19 +0100

    Require login for book_list view
    Added login requirement to book_list view.

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, "book_list.html", {"books": books})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def list_books(request):
