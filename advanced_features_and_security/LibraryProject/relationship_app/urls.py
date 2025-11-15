90db49ede639240436150c78572c1618fd0c80ce
7embola-py <yahyamorchid64@gmail.com>
2025-11-15 14:51:08 +0100

    Fix migration history, clear app errors, verify functionality

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import LibraryDetailView
+
+urlpatterns = [
+    path('', views.list_books, name='list_books'),
+
+    # Auth routes
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register_view, name='register'),
+
+    # Library detail route
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+
+
