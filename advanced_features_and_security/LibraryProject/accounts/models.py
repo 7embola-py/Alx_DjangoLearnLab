d0117b83f6481878e42b1f1f75ede4c7074df973
7embola-py <yahyamorchid64@gmail.com>
2025-11-15 15:33:44 +0100

    Remove custom manager from CustomUser model
    Removed the custom manager from the CustomUser model.

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -22,4 +22,3 @@ class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    objects = CustomUserManager()  # attach custom manager
