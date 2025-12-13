cc5c268da31f1598336feaefaa70c9004053bc71
7embola-py <yahyamorchid64@gmail.com>
2025-12-13 13:16:27 +0100

    Initial setup + core endpoints

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@!1_@@5!pmukfln0=7rnku6$*w$)v9x1540cn*d$k*t89av^yn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
 
