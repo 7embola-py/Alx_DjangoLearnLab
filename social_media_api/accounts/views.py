edef401f6e672515fabc6146603ec2da10a235a6
7embola-py <yahyamorchid64@gmail.com>
2025-12-13 12:26:59 +0100

    Initial setup + core endpoints

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from rest_framework import generics, permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -41,20 +40,26 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
     def get_object(self):
         return self.request.user
 
-class FollowUserView(APIView):
+class FollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    lookup_field = 'id'
+    lookup_url_kwarg = 'user_id'
 
-    def post(self, request, user_id):
-        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    def post(self, request, *args, **kwargs):
+        user_to_follow = self.get_object()
         if user_to_follow == request.user:
             return Response({"detail": "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
         request.user.following.add(user_to_follow)
         return Response({"detail": f"You are now following {user_to_follow.username}."}, status=status.HTTP_200_OK)
 
-class UnfollowUserView(APIView):
+class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
+    queryset = CustomUser.objects.all()
+    lookup_field = 'id'
+    lookup_url_kwarg = 'user_id'
 
-    def post(self, request, user_id):
-        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    def post(self, request, *args, **kwargs):
+        user_to_unfollow = self.get_object()
         request.user.following.remove(user_to_unfollow)
         return Response({"detail": f"You have unfollowed {user_to_unfollow.username}."}, status=status.HTTP_200_OK)
